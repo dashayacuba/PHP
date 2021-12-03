2fbeb373008ae783bb0d61319e31d8535ae04714
dashayacuba <95143360+dashayacuba@users.noreply.github.com>
2021-12-03 12:03:55 +0300

    Update passgen.php

--- passgen.php
+++ passgen.php
@@ -1,4 +1,5 @@
 <?php
+    for ($j = 0; $j < 20; $j++) {
     $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
     $pass = array(); //remember to declare $pass as an array
     $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
@@ -7,4 +8,5 @@
         $pass[] = $alphabet[$n];
     }
     echo implode($pass); //turn the array into a string
+    }
 ?>
